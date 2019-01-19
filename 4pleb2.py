f69ce7d2a538cf0c60ceb2752c3174147bb26aa0
osama-khalid <khalyd.osama@gmail.com>
2019-01-19 15:17:32 -0600

    Add files via upload

---
+++ 4pleb2.py
@@ -0,0 +1,33 @@
+#http://archive.4plebs.org/_/api/chan/thread/?board=adv&num=16627902
+#http://archive.4plebs.org/_/api/chan/index/?board=adv&page=1
+import json
+import requests
+import time as time
+allPage=[]
+i=0
+board='pol'
+while(1):
+    i=i+1
+    if i%10==0:
+        print(i)
+    while True:
+        try:
+            r=requests.get("http://archive.4plebs.org/_/api/chan/index/?board="+board+"&page="+str(i))
+            break
+        except:
+            print("Wait")
+            time.sleep(60)
+    if len(r.content)>5:
+        page=r.json()
+        for p in page:
+            allPage.append(p)
+        allPage=list(set(allPage))
+    else:
+        break
+
+
+file=open('4pleb'+'_'+board+'json','w')
+for f in allPage:
+    file.write(f+'\n')
+    
+file.close()    
