0969885acc26e836ad739e637c9637557f6f7a0c
osama-khalid <khalyd.osama@gmail.com>
2019-01-24 11:50:13 -0600

    Add files via upload

---
+++ 2itch.py
@@ -0,0 +1,8 @@
+import requests
+import sys
+import json
+import re
+import os
+import string
+import argparse
+https://api.twitch.tv/v5/videos/366985422/comments?content_offset_seconds=15190&client_id=NnFyNm1paDkzdTFpMWE5b29wN3Z4MXdpczFzMTlw base64decode
