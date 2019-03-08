8772c298999f1fcb2d4494650c4595a2ea4bb361
osama-khalid <khalyd.osama@gmail.com>
2019-03-08 11:43:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ import contractions #pip install
 
 from nltk.corpus import words             #check dictionary
 from nltk import pos_tag as posTag
+import emoji #pip install
 import re                           #elongation
-from autocorrect import spell as spellCheck      #pip install     #check spelling
+from autocorrect import spell    #pip install     #check spelling
 from nltk.tokenize import sent_tokenize #sentence tokenizer         https://www.nltk.org/api/nltk.tokenize.html also see
 import csv                              #read file    
 from datetime import datetime           #convert unix time to human time
@@ -29,31 +30,82 @@ CMUdict = cmudict.dict()      #syllable
 class preProcess(object):
     
     def __init__(self):
+        '''
+        loads urban dictionary and emoji list
+        '''
         self.ud=self.urbanLoad()
-    
+        self.emojiList=self.emojiLoad()
+        
+        
     def chanCleaner(self,post):          #clean 4archive posts
+        '''
+        cleans 4chan posts by removing the initial disclaimer
+        '''
+        
         return(post.replace('<marquee>[trigger warning]</marquee>','').replace('\\\n',' . ')[6:])
 
-    def chanDate(self,D,split=1):    #standardizes 4chan date
+    def chanDate(self,D,split=1):   
+        '''
+        standardizes 4chan date to 
+        YYYY-S
+        where S = split 
+        if split =1 S will range from 0 to 11
+        if split =6, S will range from 0 to 1
+        '''
         date_object=datetime.fromtimestamp(int(row[dateStamp]))
         return(str(date_object.year)+'-'+str((int(date_object.month)-1)//split))
 
     def voatCleaner(self,post):      #cleans voat posts
+        '''
+        cleans Voat posts by fixing escaped html
+        '''
         return(html.unescape(post).replace('\n','. ').replace('&nbsp;','.'))    
     
     def redditCleaner(self,post):      #cleans voat posts
+        '''
+        cleans reddit posts by converting newline to ".". 
+        This makes it easier for the sentence tokenizer
+        '''
         return(post.replace('\n','. '))        
     
-    def voatDate(self,D,split=1):    #standardizes voat date
+    def voatDate(self,D,split=1):   
+        '''
+        standardizes voat date to 
+        YYYY-S
+        where S = split 
+        if split =1 S will range from 0 to 11
+        if split =6, S will range from 0 to 1
+        '''
         date_object=D.split(' ')[0].split('/')
         return(str(date_object[2])+'-'+str((int(date_object[0])-1)//split))
     
     def redditDate(self,D,split=1):    
+        '''
+        standardizes reddit date to 
+        YYYY-S
+        where S = split 
+        if split =1 S will range from 0 to 11
+        if split =6, S will range from 0 to 1
+        '''
         date_object=datetime.fromtimestamp(int(row[dateStamp]))
         return(str(date_object.year)+'-'+str((int(date_object.month)-1)//split))
-
-    def urbanLoad(self,year=2019): #urban dictionary load in lowercase
-
+    
+    def emojiLoad(self):
+        '''
+        loads emoji list
+        '''
+        emojiList=[]
+        emojis=emoji.UNICODE_EMOJI
+        for e in emojis:
+            emojiList.append(e)
+            
+        return(emojiList)
+    
+    
+    def urbanLoad(self,year=2019): 
+        '''
+        loads urban dictionary in lowercase
+        '''
         #load urban dictionary
         urbanFile=open('urbandictionary.'+str(year),'r',encoding='utf-8').read().split('\n')
         #urban dictionary:
@@ -71,45 +123,119 @@ class preProcess(object):
     def reduceLengtheningSingle(self,text):    #Contracts all repetitions to 1
         pattern = re.compile(r"(.)\1{1,}")
         return pattern.sub(r"\1", text)
+
+
+    def hasElongation(self,text):         
+        '''
+        returns True if word has a letter with 3 or more repetitions
+        e.g. helllo
+        False otherwise
+        '''
+        text=text+" "
+        c=1
+        maxC=1
+        for i in range(1,len(text)):
+            if text[i]==text[i-1]:
+                c=c+1
+            else:
+                if c>maxC:
+                    maxC=c
+                c=1
+        if maxC>=3:
+            return(True)
+        else:
+            
+            return(False)
         
             
-    def wordCheck(self,word):
+    def wordOOV(self,word):           #Also for OOV Words
+        '''
+        return False if word is not OOV
+        true otherwise
+        Dictionaries   = words.word for standard english
+                            = urban dictionary
+                            = emoji List
+        '''
         if word in words.words():         #if word in dictionary
-            return(word)
+            return(False)
         if word in self.ud:                      #if word in urbandictionary
-            return(word)
-        return(None)
+            return(False)
+        if word in self.emojiList:
+            return(False)
+        return(True)
     
 
-    
+    def spellCheck(self,word):
+        '''
+        returns True if spelling error
+        False if no spelling error
+        Does not correct word, 
+        Use autocorrect.spell to correct spelling
+        limitation of Autocorrect.spell: Will map every word onto a potential word
+        
+        There's some logic behind this that I have now forgotten.
+        Oh Right! autocorrect.spell treats no spelling error and out of vocab words the same
+        e.g.
+        apple=apple
+        yoeyeyeohfifess=yoeyeyeohfifess
+        but
+        appple=apple
+        
+        '''
+        norvigWord=spell(word)          #Norvig's 21 line spell check https://impythonist.wordpress.com/2014/03/18/peter-norvigs-21-line-spelling-corrector-using-probability-theory/
+        if norvigWord==word:
+            if wordOOV(norvigWord)==False:      #Not OOV
+                return(False)             #No Spell Error
+            else:
+                return(True)               #Spell Error
+                
+        return(True)                      #Spelling Error
+        
+                
+            
+            
     def wordNormalizer(self,word):#      Does not convert i;ve to I have
+        '''
+        if a word has a elongation, removes elongation
+        returns a word if it has a elongation (and is in the dictionary)
+        or doesn't have a elongation (Can spell check later on)
+        returns None, if the word might have a elongation, but the elongation 
+        is not in the dictionary
+        '''
+        if self.hasElongation(word)==False:
+            return(word)
+        
         word=word.lower()
         
         word1=reduceLengtheningSingle(word)
         
         word2=reduceLengthening(word)
         if word1==word:                     #No double letter
-            return(wordCheck(word))
+            return(wordOOV(word))
             
-        if word==word2:                    #No contraction at all
-            return(wordCheck(word))
-        if wordCheck(word2) != None:       #word with double letter is in dictionary            
-            return(wordCheck(word2))
-        if wordCheck(word1) !=None:        #word with single letters is in dictionary          luuuuvvvv
-            return(wordCheck(word1))
-            
-        if spellCheck(word1)==spellCheck(word2):                  #   finnnnnaaaallly
-            return(spellCheck(word1))
+        if wordOOV(word2) == False:       #word with double letter is in dictionary            
+            return(wordOOV(word2))
+        if wordOOV(word1) ==False:        #word with single letters is in dictionary          luuuuvvvv
+            return(wordOOV(word1))
+
         else:
             return(None)
 
-    def deContraction(self,word): #Normalizes contractions (but doesn't work for all lower case)         #A better option would be pycontractions, but my current machine can't load the models
+    def deContraction(self,word): 
+        '''
+        Normalizes contractions (but doesn't work for all lower case)         
+        A better option would be pycontractions, but my current machine can't load the models
+        '''
         return(contractions.fix(word))
 
 
 
     
-    def sentenceTokenize(self,post):		#Returns list of sentences in Post and list of list of words
+    def sentenceTokenize(self,post):		
+        '''
+        Returns list of sentences in Post 
+        and list of list of words   #To reduce compute cost
+        '''
         sentences=[]
         word=[]
         rawSentences=sent_tokenize(post)
@@ -122,15 +248,28 @@ class preProcess(object):
                         word.append(tokens)           #all words tokenized
         return([sentences,word])
 
-    def wordTokenize(self,sentence): #Returns list of words in sentence
-        tokens = punctuations.tokenize(sentence)
-        if len(tokens)>0:
+    def wordTokenize(self,sentence): 
+        '''
+        #Returns list of words in sentence
+        '''
+        sentence=sentence.replace("'",'_')
+        tempTokens = punctuations.tokenize(sentence)
+        tokens=[]
+        if len(tempTokens)>0:
+            for t in tempTokens:
+                
+                tokens.append(t.replace("_","'"))
             return(tokens)
         else:
             return(None)        
 
 
-    def wordTokenizeCons(self,sentence):    #Conservative word Tokenizer
+    def wordTokenizeCons(self,sentence):    
+        '''
+        Conservative word Tokenizer
+        Fixes Contractions before tokenizing
+        
+        '''
         sentence=sentence.replace("'",'_')
         tempTokens = punctuations.tokenize(sentence)
         tokens=[]
@@ -147,6 +286,10 @@ class preProcess(object):
 
         
     def postFlatten(self,board):
+        '''
+        flattens a list of sentences into a single sentence
+        
+        '''
         s=""
         for p in board:
             s=s+" "+p
